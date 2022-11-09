991600809448f07b392ec81d7500751f002bced2
Abhi3070 <69385258+tron01@users.noreply.github.com>
2022-11-09 22:26:54 +0530

    linked list insert last

---
+++ c/linked1st.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+	int data;
+	struct node *next;
+	
+};
+struct node *head=NULL;
+int main(){
+	int n,em,i;
+	struct node *newnode,*temp;
+	printf("Enter the size of node:");
+	scanf("%d",&n);
+	for( i=0;i<n;i++){
+    newnode=(struct node *)malloc(sizeof(struct node *));
+    printf("Enter the Elements:");
+	scanf("%d",&em);
+	newnode->data=em;
+	newnode->next=NULL;
+    if(head==NULL){
+	 head=newnode;
+         }else{
+         	temp=head;
+         while(temp->next!=NULL){
+         	temp=temp->next;
+		 }
+		 temp->next=newnode;
+		 
+		 
+		 }
+	}
+	printf("---LinkedList---\n");
+	temp=head;
+	while(temp!=NULL){
+		printf("%d\n",temp->data);
+		temp=temp->next;
+	}
+
+	
+	
+	return 0;
+}
