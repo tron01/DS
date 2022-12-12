cbf6dac3f8ee7f28d07e7977c6ebce0c47e3c956
tron01 <69385258+tron01@users.noreply.github.com>
2022-12-12 20:38:38 +0530

    fix

--- c/ddlnked.c
+++ c/ddlnked.c
@@ -1,179 +1,239 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
-	struct node *prev;
-	struct node *next;
-	int data;
+struct node
+{
+  struct node *prev;
+  struct node *next;
+  int data;
 };
 
-struct node *new=NULL,*ptr=NULL,*head=NULL, *nextptr=NULL, *prevptr=NULL;
+struct node *new = NULL, *ptr = NULL, *head = NULL;
 
-void insertion_last();
-void delete_last();
-void insertion_beginning();
-void delete_beginning();
-void display();
-void insert_after_key();
-void delete();
-void create_node();
+void insert_last ();
+void delete_last ();
+void insert_beginning ();
+void delete_beginning ();
+void display ();
+void insert_after_key ();
+void delete_between ();
+void create_node ();
 
-int  main() {
-	int opt;
-	do {
-		printf("\nChoose the operation number : \n");
-		printf("1.Insertion last \n2.Delete last \n3.Insertion Beginning \n4.Delete beginning \n5.Insert after key \n6.Delete \n7.Display \n0.Exit : ");
-		scanf("%d",&opt);
-		switch(opt) {
-			case 1: insertion_last();
-							break;
-			case 2: delete_last();
-							break;
-			case 3: insertion_beginning();
-							break;
-			case 4: delete_beginning();
-							break;
-			case 5: insert_after_key();
-							break;
-			case 6: delete();
-							break;
-			case 7: display();
-							break;
-			case 0:
-			default:break;
+int main ()
+{
+  int opt;
+  do
+    {
+      printf ("\nEnter the choice: \n");
+      printf("1.Insertion Beginning \n2.Inserting after key \n3.Insertion last \n4.Delete beginning \n5.Delete In between \n6.Delete last \n7.Display \n0.Exit : ");
+      scanf ("%d", &opt);
+      switch (opt)
+	{
+	case 1:
+	    insert_beginning ();
+	  break;
+	case 2:
+	  insert_after_key();
+	  break;
+	case 3:
+	   insert_last ();
+	  break;
+	case 4:
+	  delete_beginning ();
+	  break;
+	case 5:
+	   delete_between ();
+	  break;
+	case 6:
+	   delete_last ();
+	  break;
+	case 7:
+	  display ();
+	  break;
+	case 0:
+	default:
+	  break;
+	}
+    }
+  while (opt != 0);
+  return 0;
+}
+
+void
+create_node ()
+{
+  new = (struct node *) malloc (sizeof (struct node));
+  printf ("Enter the data to be inserted : ");
+  scanf ("%d", &new->data);
+  new->next = NULL;
+  new->prev = NULL;
+}
+
+void
+insert_last ()
+{
+  create_node ();
+  if (head == NULL)
+    {
+      head = new;
+    }
+  else
+    {
+      ptr = head;
+      while (ptr->next != NULL)
+	{
+	  ptr = ptr->next;
+	}
+      ptr->next = new;
+      new->prev = ptr;
+    }
+}
+
+void
+insert_beginning ()
+{
+  create_node ();
+  if (head == NULL)
+    {
+      head = new;
+    }
+  else
+    {
+      head->prev = new;
+      new->next = head;
+      head = new;
+    }
+}
+
+void
+insert_after_key ()
+{
+  int key;
+  create_node ();
+  printf ("Enter the key where the node to be inserted : ");
+  scanf ("%d", &key);
+  ptr = head;
+ while(ptr->next!=NULL && ptr->data!=key)
+		{
+		ptr=ptr->next;
 		}
-	} while(opt!=0);
+	if(ptr->next==NULL)
+		{
+		ptr->next=new;
+		ptr->prev=ptr;
+		}
+	else
+	{
+	new->next = ptr->next;
+      new->prev = ptr;
+      ptr->next = new;
+      ptr->next->prev = new;
+	}
+}
+void
+delete_beginning ()
+{
+  if (head == NULL)
+    {
+      printf ("Empty List");
+    }
+  else if (head->next == NULL)
+    {
+      head = NULL;
+      free (head);
+      printf ("NODE DELETED");
+    }
+  else
+    {
+      ptr = head;
+      head = head->next;
+      head->prev = NULL;
+      free (ptr);
+      printf ("NODE DELETED");
+    }
+}
+
+void
+delete_last ()
+{
+  if (head == NULL)
+    {
+      printf ("Empty List \n");
+    }
+  else if (head->next == NULL)
+    {
+      head = NULL;
+      free (head);
+      printf ("\nnode deleted\n");
+    }
+  else
+    {
+      ptr = head;
+      while (ptr->next != NULL)
+	{
+	  ptr = ptr->next;
+	}
+      ptr->prev->next = NULL;
+      free (ptr);
+      printf ("\nnode deleted\n");
+    }
+}
+
+void
+delete_between ()
+{
+  int key;
+  printf ("Enter the key where the node to be deleted : ");
+  scanf ("%d", &key);
+  if (head->data == key)
+    {
+      delete_beginning ();
+    }
+ else if (head == NULL)
+    {
+      printf ("\nlist is empty\n");
+    }
+  else
+    {
+      ptr = head;
+      while (ptr->next != NULL && ptr->data != key)
+	{
+	  ptr = ptr->next;
+	}
 	
-	return 0;
-}
-
-void create_node() {
-	new = (struct node*)malloc(sizeof(struct node));
-	printf("Enter the data to be inserted : ");
-	scanf("%d",&new->data);
-	new->next = NULL;
-	new->prev = NULL;
-}
-
-void insertion_last() {
-	create_node();
-	if(head == NULL) {
-		head = new;
-	}
-	else {
-		ptr=head;
-		while(ptr->next!=NULL){
-			ptr = ptr->next;
-		}
-		ptr->next = new;
-		new->prev = ptr;
-	}
-}
-
-void insertion_beginning() {
-	create_node();
-	if(head == NULL){
-		head = new;
-	} else {
-		head->prev=new;
-		new->next = head;
-		head = new;
-	}
-}
-
-void insert_after_key() {
-	int key;
-	create_node();
-	printf("Enter the key where the node to be inserted : ");
-	scanf("%d",&key);
-	ptr=head;
-	while(ptr->data != key && ptr->next != NULL){
-		ptr = ptr->next;
-	}
-	if(ptr->data == key){
-		new->next = ptr->next;
-		new->prev = ptr;
-		ptr->next = new;
-		ptr->next->prev = new;
-	}
-	else {
-		printf("The key is not found");
-	}
-}
-
-void delete_beginning() {
-	if(head == NULL){
-		printf("Empty List");
-	}
-	else if(head->next == NULL) {
-		head = NULL;
-		free(head);
-		printf("NODE DELETED");
-	}
-	else {
-		ptr=head;
-		head = head->next;
-		head->prev = NULL;
-		free(ptr);
-		printf("NODE DELETED");
-	}
-}
-
-void delete_last() {
-	if(head == NULL) {
-		printf("Empty List \n");
-	} 
-	else if(head->next == NULL) {
-		head = NULL;
-		free(head);
-		printf("\nnode deleted\n");  
-	}
-	else {
-		ptr = head;
-		while(ptr->next!=NULL){
-			ptr = ptr->next;
-		}
-		ptr->prev->next = NULL;
-		free(ptr);
-		printf("\nnode deleted\n");  
-	}
-}
-
-void delete() {
-	int key;
-	printf("Enter the key where the node to be deleted : ");
-	scanf("%d",&key);
-	if(head->data == key){
-		delete_beginning();
-	}
-	else {
-		ptr=head;
-		while(ptr->data != key && ptr->next != NULL){
-			ptr = ptr->next;
-		}
-		if(ptr->data == key) {
-			ptr->prev->next=ptr->next;
-			ptr->next->prev=ptr->prev;
-			free(ptr);
-			printf("NODE DELETED");
-		}
-		else {
-			printf("No data found");
-		}
-	}
-}
-
-void display() {
-	if(head == NULL) {
-		printf("Empty List \n");
-	} else {
-		ptr = head;
-		while(ptr!=NULL){
-			printf("%d ",ptr->data);
-			ptr = ptr->next;
-		}
-		printf("\n");
-	}
-}
+	  
 	
 	
+      if (ptr == NULL)
+	printf ("\nelement not found\n");
+      else
+	 if(ptr->next ==NULL) {
+	 	delete_last ();
+	 }else {
+	    ptr->prev->next = ptr->next;
+	    if (ptr->next != NULL)
+	    ptr->next->prev = ptr->prev;
+	    else
+	    ptr->next = NULL;
+	    free(ptr);
+	printf("node deleted");
+    }}
+}
+
+void display ()
+{
+  if (head == NULL)
+    {
+      printf ("Empty List \n");
+    }
+  else
+    {
+      ptr = head;
+      while (ptr != NULL)
+	{
+	  printf ("%d ", ptr->data);
+	  ptr = ptr->next;
+	}
+      printf ("\n");
+    }
+}
